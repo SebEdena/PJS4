9b6397bfe5b7de13c16449b804799b6fac51a708
viguier.sebastien@orange.fr <sebastien.viguier@etu.parisdescartes.fr>
2017-01-30 13:59:37 +0100

    Ajouté dossier jeu initial

---
+++ Jeu/Assets/InputHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    private string verticalAxis = "Vertical";
+    private string horizontalAxis = "Horizontal";
+    private PlayerController pc;
+    private float vValue;
+    private float hValue;
+
+    public float speedMult;
+
+
+    // Use this for initialization
+    void Start () {
+        vValue = 0f;
+        hValue = 0f;
+        pc = GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        vValue = Input.GetAxis(verticalAxis);
+        hValue = Input.GetAxis(horizontalAxis);
+	}
+
+    void FixedUpdate() {
+        Vector3 direction;
+        direction = (Vector3.forward * vValue + Vector3.right * hValue) * speedMult * Time.deltaTime;
+        pc.Move(direction);
+    }
+}
---
+++ Jeu/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxRotation;
+    public float speedReduction;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Vector3 direction)
+    {
+        direction.Set(direction.x, rb.velocity.y, direction.z);
+        rb.transform.rotation = Quaternion.FromToRotation(rb.transform.position, direction);
+        rb.velocity = direction;
+    }
+}
