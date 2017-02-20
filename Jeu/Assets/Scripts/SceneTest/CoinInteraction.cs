ebd3d88eb9bad077d9663f4da7562fe7f8c669ee
Sebastien VIGUIER <viguier.sebastien@orange.fr>
2017-02-20 14:32:07 +0100

    CHANGEMENT MAJEUR SUSPECT MAGUEULE

---
+++ Jeu/Assets/Scripts/SceneTest/CoinInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinInteraction : MonoBehaviour {
+
+    public float rotateSpeedRatio;
+
+    public float jumpPower;
+
+    private static float fullRotate = 360f;
+
+    private float rotation;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rotation = 0f;
+	}
+
+    void FixedUpdate()
+    {
+        float rotationOffset = fullRotate * rotateSpeedRatio * Time.deltaTime;
+        rotation += rotationOffset;
+        if (rotation > fullRotate)
+        {
+            Jump();
+            rotation = rotation % fullRotate;
+        }
+        Quaternion rota = Quaternion.Euler(rb.rotation.eulerAngles + Vector3.up * rotationOffset);
+        rb.MoveRotation(rota);
+    }
+
+    private void Jump()
+    {
+        rb.velocity.Set(rb.velocity.x, rb.velocity.y + jumpPower, rb.velocity.z);
+    }
+}
---
+++ Jeu/Assets/Scripts/SceneTestMat/IASimplev2.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IASimplev2 : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	private Vector3 initial;
+
+	public float pathLength;
+
+	public float speed;
+
+    public float jumpPower;
+
+    private float rotation;
+
+	private int direction;
+
+	private bool canJump = false;
+
+	void Awake(){}
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		initial = transform.position;
+		direction = 1;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (transform.position.z < initial.z || transform.position.z > (initial.z + pathLength)) {
+			rotation += 180f * Time.deltaTime;
+			if (rotation > 180f) {
+				float diff = 180f - rotation;
+
+				Quaternion rota = Quaternion.Euler(rb.rotation.eulerAngles.x, 
+					rb.rotation.eulerAngles.y + (180f * Time.deltaTime) - diff, 
+					rb.rotation.eulerAngles.z);
+				rb.MoveRotation (rota);
+				rotation = 0f;
+				direction *= (-1);
+				transform.position += Vector3.forward * direction * speed * Time.deltaTime;
+			} else
+			{
+				Quaternion rota = Quaternion.Euler(rb.rotation.eulerAngles.x,
+					rb.rotation.eulerAngles.y + (180f * Time.deltaTime),
+					rb.rotation.eulerAngles.z);
+				rb.MoveRotation(rota);
+			}
+		} else {
+			transform.position += Vector3.forward * direction * speed * Time.deltaTime;
+			Jump ();
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.CompareTag("Player")){
+			col.gameObject.GetComponent<PlayerController>().Die();
+		}
+		if (col.gameObject.CompareTag("Floor"))
+		{
+			canJump = true;
+		}
+	}
+
+	public void Jump()
+	{
+		if (canJump) {
+			rb.velocity += Vector3.up * getYVelocity ();
+			if (!Mathf.Approximately (rb.velocity.y, 0f))
+				canJump = false;
+		}
+	}
+
+	private float getYVelocity()
+	{
+		return jumpPower;
+	}
+}
